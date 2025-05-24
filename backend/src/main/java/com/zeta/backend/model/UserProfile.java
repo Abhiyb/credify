ea64e34b5673996745cd45f3fd561a10b87bd448
Abhishekhzeta <abhishekhy@zeta.tech>
2025-05-24 16:57:14 +0530

    added backend file

--- backend/src/main/java/com/zeta/backend/model/UserProfile.java
+++ backend/src/main/java/com/zeta/backend/model/UserProfile.java
@@ -45,11 +45,7 @@ public class UserProfile {
     @Column(name = "annual_income", nullable = false)
     private Double annualIncome;
 
-    @NotBlank(message = "Password is required")
-    @Pattern(
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,}$",
-            message = "Password must be at least 8 characters long and include one uppercase letter, one lowercase letter, one digit, and one special character (@#$%^&+=!)"
-    )
+
     @Column(nullable = false)
     private String password;
 
